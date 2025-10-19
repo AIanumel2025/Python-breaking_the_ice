ccc83de27e236f0ff283c15fbf8cee76e863f295
Tony Anumel <aanumel2025@gmail.com>
2025-10-19 15:54:46 +0100

    Update Collatz sequence.py

--- Fundamentals/Collatz sequence.py
+++ Fundamentals/Collatz sequence.py
@@ -1,3 +1,9 @@
+# A typical Collatz sequence code: Code allows input of a positive number. 
+# If the number is even it is divided by 2.
+# If odd, it is multiplied by 3 with one added to it
+
+# Defined a function collatz(number) and utilized if conditionals to apply the rule
+# Looped the program until final number outputted was 1
 
 def collatz(number):
     if number % 2 == 0:
@@ -8,9 +14,9 @@ def collatz(number):
         print(3 * number + 1)
         return 3 * number + 1
 
-
-
 while True:
     print('Enter your number here')
     collatz(int(input()))
     
+
+# Feedback? Thoughts, suggestions all welcome!
